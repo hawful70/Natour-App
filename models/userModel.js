553f7d56a10a7f2be8240eeb2a12b4f444d90c42
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-08-24 22:23:17 +0700

    add sign up function

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-  mame: {
+  name: {
     type: String,
     required: [true, 'Please tell us your name']
   },
