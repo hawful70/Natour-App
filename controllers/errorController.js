2a6d042f6942f0297a194a2f3894626157989535
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-08-24 17:05:08 +0700

    catch error outside of express

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -36,6 +36,8 @@ const sendErrorProd = (err, res) => {
 
     // Programming or other unknown error: don't leak error details
   } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
     // 2) Send generic message
     res.status(500).json({
       status: 'error',
