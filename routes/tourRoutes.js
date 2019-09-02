075276798e1d51500a08cf7cb3733d34e7bd6930
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-09-02 18:02:52 +0700

    apply merge param to review routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
-//router.param('id', tourController.checkID);
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
-  );
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
