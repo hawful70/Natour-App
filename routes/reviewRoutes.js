685edc84bf31b5ac7f6b020c6de3d71a85c91fe8
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-09-02 21:13:26 +0700

    implement factory handler function: delete

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -13,4 +13,11 @@ router
     reviewController.createReview
   );
 
+router
+  .route('/:id')
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
+
 module.exports = router;
