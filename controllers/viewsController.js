7421ad09a08988f2e018be5d8835f6629b2044bd
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-09-07 21:27:01 +0700

    Building the tour overview

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
