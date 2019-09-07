74c180734cc17431676a93b88bd9ca6adcbcc429
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-09-07 23:47:42 +0700

    Building tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -11,8 +11,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hikker Tour'
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour
+  });
+});
