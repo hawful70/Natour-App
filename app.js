ad02f4d69eb1ae66ee966173348eec00abcfc479
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-09-07 16:15:03 +0700

    create base template

--- app.js
+++ app.js
@@ -69,7 +69,10 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The forest hikker',
+    user: 'Jonas'
+  });
 });
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
