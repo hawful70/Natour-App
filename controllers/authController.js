e6b606cf8a3817599dfebf023a586027240bfea9
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-09-08 17:27:25 +0700

    implement login functionality, refactor code js, add bundle lib to minimize all js file

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,6 +66,10 @@ exports.protect = catchAsync(async (req, res, next) => {
     token = req.headers.authorization.split(' ')[1];
   }
 
+  if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+
   if (!token) {
     return next(
       new AppError('You are not logged in! Please log in to get access.', 401)
@@ -93,6 +97,36 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // 1) verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
+
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+
+      // 3) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+
+      // THERE IS A LOGGED IN USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
+  }
+  next();
+};
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,43 +1,43 @@
 /* eslint-disable */
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+export const displayMap = locations => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiaGF3ZnVsNzAiLCJhIjoiY2swYW1oNHZwMGpobjNtcGVnMnJ3YmF2ciJ9.N0ASTtwyC8gDvq7LKYWquQ';
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiaGF3ZnVsNzAiLCJhIjoiY2swYW1oNHZwMGpobjNtcGVnMnJ3YmF2ciJ9.N0ASTtwyC8gDvq7LKYWquQ';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/hawful70/ck0amkxc2301x1cn25xt5rfbg',
+    scrollZoom: false
+  });
 
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/hawful70/ck0amkxc2301x1cn25xt5rfbg',
-  scrollZoom: false
-});
+  const bounds = new mapboxgl.LngLatBounds();
 
-const bounds = new mapboxgl.LngLatBounds();
+  locations.forEach(loc => {
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-locations.forEach(loc => {
-  const el = document.createElement('div');
-  el.className = 'marker';
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom'
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom'
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    new mapboxgl.Popup({
+      offset: 30
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
 
-  new mapboxgl.Popup({
-    offset: 30
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-    .addTo(map);
+    bounds.extend(loc.coordinates);
+  });
 
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100
-  }
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100
+    }
+  });
+};
