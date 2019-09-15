8c26ad3b6234fae955df3e448b8924663a0b329c
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-09-15 11:35:35 +0700

    change file to prepare deploy

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,7 +6,7 @@ const stripe = Stripe('pk_test_Cf21R6nQGfVFv8MbeZvueKwJ00ExbjMjMB');
 export const bookTour = async tourId => {
   try {
     const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+      `/api/v1/bookings/checkout-session/${tourId}`
     );
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
