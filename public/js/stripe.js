ad7bfa97d1846be66143b75c64c0d3b02f3215f5
VinhDangQuang <45581115+VinhDangQuang@users.noreply.github.com>
2019-09-15 17:19:49 +0700

    implement alert middleware

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -5,9 +5,7 @@ const stripe = Stripe('pk_test_Cf21R6nQGfVFv8MbeZvueKwJ00ExbjMjMB');
 
 export const bookTour = async tourId => {
   try {
-    const session = await axios(
-      `/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
     });
